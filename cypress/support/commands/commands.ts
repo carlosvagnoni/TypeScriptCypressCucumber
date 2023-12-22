55a8cf946318a13b940ee73c21968bb03f0c6165
unknown <carlosvagnoni11@gmail.com>
2023-12-22 13:02:32 +0100

    Implement SonarQube suggestions for code improvements

--- cypress/support/commands/commands.ts
+++ cypress/support/commands/commands.ts
@@ -4,39 +4,38 @@
  */
 
 // Definition of custom types
-type Selector = string;
-type ExpectedText = string;
+type CustomChainable = Cypress.Chainable<string | number | string[]>;
 
 // Custom commands
-Cypress.Commands.add('clickElement', (elementLocator: Selector): Cypress.Chainable<JQuery<HTMLElement>> => {
+Cypress.Commands.add('clickElement', (elementLocator: string): Cypress.Chainable<JQuery<HTMLElement>> => {
     return cy.get(elementLocator).should('be.visible').click() as Cypress.Chainable<JQuery<HTMLElement>>;
 });
 
-Cypress.Commands.add('waitForElementBeVisible', (elementLocator: Selector): Cypress.Chainable<JQuery<HTMLElement>> => {
+Cypress.Commands.add('waitForElementBeVisible', (elementLocator: string): Cypress.Chainable<JQuery<HTMLElement>> => {
     return cy.get(elementLocator).should('be.visible') as Cypress.Chainable<JQuery<HTMLElement>>;
 });
 
-Cypress.Commands.add('waitForElementExist', (elementLocator: Selector): Cypress.Chainable<JQuery<HTMLElement>> => {
+Cypress.Commands.add('waitForElementExist', (elementLocator: string): Cypress.Chainable<JQuery<HTMLElement>> => {
     return cy.get(elementLocator).should('exist') as Cypress.Chainable<JQuery<HTMLElement>>;
 });
 
-Cypress.Commands.add('waitForElementNotBeVisible', (elementLocator: Selector): Cypress.Chainable<JQuery<HTMLElement>> => {
+Cypress.Commands.add('waitForElementNotBeVisible', (elementLocator: string): Cypress.Chainable<JQuery<HTMLElement>> => {
     return cy.get(elementLocator).should('not.be.visible') as Cypress.Chainable<JQuery<HTMLElement>>;
 });
 
-Cypress.Commands.add('waitForElementInnerTextToBe', (elementLocator: Selector, expectedText: ExpectedText): Cypress.Chainable<JQuery<HTMLElement>> => {
+Cypress.Commands.add('waitForElementInnerTextToBe', (elementLocator: string, expectedText: string): Cypress.Chainable<JQuery<HTMLElement>> => {
     return cy.get(elementLocator).should('have.text', expectedText, { timeout: 1000 }) as Cypress.Chainable<JQuery<HTMLElement>>;
 });
 
-Cypress.Commands.add('verifyElementText', (elementLocator: Selector, expectedText: ExpectedText): Cypress.Chainable<string> => {
+Cypress.Commands.add('verifyElementText', (elementLocator: string, expectedText: string): Cypress.Chainable<string> => {
     return cy.get(elementLocator).invoke('text').should('equal', expectedText) as Cypress.Chainable<string>;
 });
 
-Cypress.Commands.add('verifyElementTextValue', (elementLocator: Selector, expectedText: ExpectedText): Cypress.Chainable<string | number | string[]> => {
-    return cy.get(elementLocator).invoke('val').should('equal', expectedText) as Cypress.Chainable<string | number | string[]>;
+Cypress.Commands.add('verifyElementTextValue', (elementLocator: string, expectedText: string): Cypress.Chainable<string | number | string[]> => {
+    return cy.get(elementLocator).invoke('val').should('equal', expectedText) as CustomChainable;
 });
 
-Cypress.Commands.add('verifyAlertText', (stub: void, expectedText: ExpectedText) => {
+Cypress.Commands.add('verifyAlertText', (stub: void, expectedText: string) => {
     expect(stub).to.have.been.calledWith(expectedText)
 })
 
@@ -46,11 +45,11 @@ Cypress.Commands.add('submitAlert', () => {
     });
 })
 
-Cypress.Commands.add('enterText', (elementLocator: Selector, text: string): Cypress.Chainable<JQuery<HTMLElement>> => {
+Cypress.Commands.add('enterText', (elementLocator: string, text: string): Cypress.Chainable<JQuery<HTMLElement>> => {
     return cy.get(elementLocator).type(text) as Cypress.Chainable<JQuery<HTMLElement>>;
 })
 
-Cypress.Commands.add('verifyCurrentUrl', (expectedUrl: ExpectedText): Cypress.Chainable<string> => {
+Cypress.Commands.add('verifyCurrentUrl', (expectedUrl: string): Cypress.Chainable<string> => {
     return cy.url().should('eq', expectedUrl) as Cypress.Chainable<string>;
 })
 
@@ -58,13 +57,13 @@ Cypress.Commands.add('verifyCurrentUrl', (expectedUrl: ExpectedText): Cypress.Ch
 declare global {
     namespace Cypress {
         interface Chainable {
-            clickElement(elementLocator: Selector): Chainable<JQuery<HTMLElement>>
-            waitForElementBeVisible(elementLocator: Selector): Chainable<JQuery<HTMLElement>>
-            waitForElementExist(elementLocator: Selector): Chainable<JQuery<HTMLElement>>
-            waitForElementNotBeVisible(elementLocator: Selector): Chainable<JQuery<HTMLElement>>
-            waitForElementInnerTextToBe(elementLocator: Selector, expectedText: ExpectedText): Chainable<JQuery<HTMLElement>>
+            clickElement(elementLocator: string): Chainable<JQuery<HTMLElement>>
+            waitForElementBeVisible(elementLocator: string): Chainable<JQuery<HTMLElement>>
+            waitForElementExist(elementLocator: string): Chainable<JQuery<HTMLElement>>
+            waitForElementNotBeVisible(elementLocator: string): Chainable<JQuery<HTMLElement>>
+            waitForElementInnerTextToBe(elementLocator: string, expectedText: string): Chainable<JQuery<HTMLElement>>
             verifyElementText(elementLocator: string, expectedText: string): Chainable<string>
-            verifyElementTextValue(elementLocator: string, expectedText: string): Chainable<string | number | string[]>
+            verifyElementTextValue(elementLocator: string, expectedText: string): CustomChainable
             verifyAlertText(stub: void, expectedText: string): Chainable<void>
             submitAlert(): Chainable<void>
             enterText(elementLocator: string, text: string): Chainable<JQuery<HTMLElement>>
